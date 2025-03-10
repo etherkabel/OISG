001955482571ebf328c58292800b3da5641b0c09
medveed <mihailnagr063@gmail.com>
2025-03-10 15:27:18 +0600

    dimple sdl setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 #include <cstdio>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_video.h>
+#include <SDL3/SDL_init.h>
 
 int main() {
-    printf("Hello, World!\n");
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window *win = SDL_CreateWindow("Test", 640, 480, 0);
+    SDL_Renderer *ren = SDL_CreateRenderer(win, "rend");
+
+    SDL_RenderClear(ren);
+    SDL_RenderPresent(ren);
+
+    SDL_Delay(5000);
+
 
     return 0;
 }
