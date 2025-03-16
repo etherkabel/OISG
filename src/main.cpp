63da775f4624b39b69280de25d3f98fc7ef40484
medveed <mihailnagr063@gmail.com>
2025-03-16 12:28:32 +0600

    Add Label

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include "SDL3/SDL_blendmode.h"
+#include <SDL3/SDL.h>
 #include "SDL3/SDL_error.h"
 #include "SDL3/SDL_events.h"
 #include "SDL3/SDL_keyboard.h"
@@ -12,18 +12,22 @@
 #include "SDL3/SDL_timer.h"
 #include "utils/Clock.h"
 #include "SDL3_image/SDL_image.h"
-#include <SDL3/SDL.h>
 #include <SDL3/SDL_video.h>
 #include <SDL3/SDL_init.h>
+#include <string>
+#include "SDL3_ttf/SDL_ttf.h"
 #include "utils/Logger.h"
+#include "engine/gui/Label.h"
 
 int main() {
     Logger log("main", Logger::DEBUG);
     if (!SDL_Init(SDL_INIT_VIDEO)) {
         log.stream(Logger::FATAL) << "SDL Initialization failed: " << SDL_GetError() << endl;
     }
+    TTF_Init();
     int start = SDL_GetTicks();
-    SDL_Window *win = SDL_CreateWindow("Test", 640, 480, SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIDDEN);
+    SDL_SetHint(SDL_HINT_VIDEO_X11_NET_WM_BYPASS_COMPOSITOR, "0");
+    SDL_Window *win = SDL_CreateWindow("Test", 640, 480, SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIDDEN | SDL_WINDOW_OPENGL);
     if (!win) {
         log.stream(Logger::FATAL) << "Window CREATION!!!!!! failed: " << SDL_GetError() << endl;
         SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "SDL Error", SDL_GetError(), NULL);
@@ -36,12 +40,21 @@ int main() {
         return 1;
     }
     log.stream(Logger::INFO) << "Using renderer: " << SDL_GetRendererName(ren) << endl;
-    if (!SDL_SetRenderVSync(ren, false)) {
+    if (!SDL_SetRenderVSync(ren, true)) {
         log.stream(Logger::WARN) << "VSync not supported: " << SDL_GetError() << endl;
     }
     SDL_ShowWindow(win);
     log.debug("Initialization complete");
     log.stream(Logger::INFO) << "SDL initialization took " << SDL_GetTicks() - start << "ms" << endl;
+
+    TTF_TextEngine *ttfEngine = TTF_CreateRendererTextEngine(ren);
+    TTF_Font *font = TTF_OpenFont("assets/fonts/JetBrainsMonoNL-Regular.ttf", 24);
+    if (!ttfEngine or !font) {
+        log.stream(Logger::FATAL) << "Couldnt create TTF " << SDL_GetError();
+        return 1;
+    }
+
+    Label fpsLabel = Label(ttfEngine, font);
 
     SDL_Surface *bmp = IMG_Load("assets/bottle.png");
     if (!bmp) {
@@ -55,11 +68,9 @@ int main() {
     SDL_GetTextureSize(tex, &rect.w, &rect.h);
     rect.h *= 8;
     rect.w *= 8;
-    int numKeys;
-    const bool *keys = SDL_GetKeyboardState(&numKeys);
-    log.stream(Logger::INFO) << "Number of keys: " << numKeys << endl;
+    const bool *keys = SDL_GetKeyboardState(NULL);
 
-    Clock clk(100000);
+    Clock clk(120);
     SDL_Event e;
     bool running = true;
     while (running) {
@@ -78,12 +89,13 @@ int main() {
         } else if (keys[SDL_SCANCODE_RIGHT]) {
             rect.x += 4;
         }
-        SDL_SetRenderDrawColor(ren, 0, 200, 0, 255);
+        SDL_SetRenderDrawColor(ren, 0, 100, 0, 255);
         SDL_RenderClear(ren);
         SDL_RenderTexture(ren, tex, NULL, &rect);
+        fpsLabel.format("FPS: %f", clk.getFPS());
+        fpsLabel.render(8, 8);
         SDL_RenderPresent(ren);
         clk.update();
-        log.stream(Logger::DEBUG) << "FPS: " << clk.getFPS() << " \tFrame time: \t" << clk.getFrameTime() << endl;
     }
 
     SDL_DestroyWindow(win);
